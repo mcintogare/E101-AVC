36a52bd593a412c29ef0dd5b8b400955d2ac8dd9
MetalicHead <66203380+MetalicHead@users.noreply.github.com>
2020-06-12 01:02:38 +1200

    Combined Completion V.1.0 into class format

--- robot2.hpp
+++ robot2.hpp
@@ -38,33 +38,76 @@ public:
 	double offset(ImagePPM cameraView);
 };
 
+
+/**
+ * This function returns the brightness of the pixel corresponding to
+ * pixIndex.
+ * 
+ * @param cameraView - an ImagePPM object representing the robot's POV
+ * @param pixIndex - the position of the pixel along the perimeter of
+ *                   cameraView. e.g. pixIndex = 0 for the pixel in the
+ *                   bottom-left hand corner (from the robot's POV).
+ * @returns the brightness of the specified pixel
+ */
+int brightness(ImagePPM cameraView, int pixIndex) {
+	int perimeterPixCount = 2 * (cameraView.width + cameraView.height) - 4;
+
+	// Indices of three important pixels
+	int topLeft = cameraView.height - 1;
+	int topRight = cameraView.height + cameraView.width - 2;
+	int bottomRight = 2 * cameraView.height + cameraView.width - 3;
+
+	if (pixIndex < topLeft) { // If pixel on left edge
+		return get_pixel(cameraView, -pixIndex + topLeft, 0, 3);
+	} else if (topLeft <= pixIndex && pixIndex < topRight) { // If pixel on top edge
+		return get_pixel(cameraView, 0, pixIndex - topLeft, 3);
+	} else if (topRight <= pixIndex && pixIndex < bottomRight) { // If pixel on right edge
+		return get_pixel(cameraView, pixIndex - topRight, cameraView.width - 1, 3);
+	} else { // If pixel on bottom edge
+		return get_pixel(cameraView, cameraView.height - 1, -pixIndex + perimeterPixCount, 3);
+	}
+}
+
+/**
+ * This function iterates over the pixels of the perimeter of
+ * cameraView, starting from the bottom-left hand corner (from the
+ * robot's POV). It calculates the distance between the average position
+ * of white pixels along this perimeter and the perimeter pixel directly
+ * in front of the robot. However, it only considers the first group of
+ * white pixels it finds, as this group will be the one that it will try
+ * to follow.
+ * 
+ * @param cameraView - an ImagePPM object representing the robot's FOV
+ * @returns the aforementioned distance/offset
+ */
 double Robot::offset(ImagePPM cameraView){
 	// Initialise variables to help calculate the error correction 
 	double error = 0.0;
 	double totWhitePix = 0.0;
+	
+	// Number of pixels on the image's perimeter
+	int perimeterPixCount = 2 * (cameraView.width + cameraView.height) - 4;
 
-	// Create a for loop that runs 150 times for each pixel in a row
-	for(int currRow = 0; currRow < cameraView.width; currRow++){
-		/* DEVELOPER COMMENTS
-		 * 
-		 * cameraView is the picture in ppm format we just took
-		 * i is current pixel in the row
-		 * 3 so we can get all 3 RGB values */
-		int middlePix = get_pixel(cameraView, cameraView.height / 2, currRow, 3);
+	for(int pixIndex = 0; pixIndex < perimeterPixCount; pixIndex++){
+		int currPixBrightness = brightness(cameraView, pixIndex);
 
 		// If the pixel value is greater than 250 consider the pixel as white
-		if(middlePix > 250){
-			// Add the currRow to the error so we can find the average (mid point) of the current white pixels
-			error += currRow;
-			// Add one to the total amount of white pixels
-			totWhitePix += 1;
-		}
+		if(currPixBrightness > 250){
+			error += pixIndex;
+		 	totWhitePix += 1;
+		// Break loop if a non-white pixel is found when at least one white pixel has already been found
+		} else if (totWhitePix != 0) { break; }
 	}
 
-	// Find the middle index positioing of the white pixels
-	double currMiddleWhitePix = error / totWhitePix;
-	// Take away 75 as that is half of the image width to find the error amount
-	return currMiddleWhitePix - 75;
+	// Index where offset = 0
+	int relativeIndex = cameraView.height + (cameraView.width - 1) / 2 - 1;
+
+	// Calculate average index of all white pixels found
+	// If no white pixles were found, set this to a large number so that
+	// the robot turns very quickly.
+	double avWhiteIndex = (totWhitePix != 0) ? error / totWhitePix
+											: 1000;
+	return avWhiteIndex - relativeIndex;
 }
 
 void Robot::runRobot() {
@@ -72,16 +115,7 @@ void Robot::runRobot() {
 	if (initClientRobot() !=0){
 		std::cout<<" Error initializing robot"<<std::endl;
 	}
-	// Initialise multiple variables to determine current speed
-	
-    //double vLeft = 23.0;
-    //double vRight = 23.0;
-	//double v_go = 23.0;
-	//double kp = 0.1; // Create a constant to help caculate change in speed
-   
-   
-   
-   
+
 	double vLeft = getvLeft();
 	double vRight = getvRight();
     double dV = vLeft - vRight; // Difference between the two speeds
@@ -93,13 +127,6 @@ void Robot::runRobot() {
 		
 		// Save the errorCorrection given by the function offset
 		double errorCorrection = offset(cameraView);
-		std::cout<<"errorCorrection "<<errorCorrection<<std::endl;
-		/* DEVELOPER COMMENTS 
-		 * An important thing to note is that the initial dV is calculated via vLeft - vRight
-		 * So, if the vRight is greater than vLeft, the difference would be negative. Therefore,
-		 * when we go to calculate the new vRight, we - dV as - a negative number gives a positive number.
-		 * And if vLeft is greater than vRight, dV is positive so we can just add that to vLeft */
-
 		// Caculate the difference between the left and right speeds
 		dV = kp * errorCorrection;
 		// Change the speeds based off the difference
@@ -113,3 +140,6 @@ void Robot::runRobot() {
 		usleep(10000);
 	} //while
 }
+
+
+
